e4b51d3df1ca267ab2d864be39ba9a2778d7caef
Justin Carrier <2162774@csfoy.ca>
2023-10-19 15:48:32 -0400

    Ajout des boutons precedent, suivant et arret

--- js/visionneuse.js
+++ js/visionneuse.js
@@ -6,6 +6,7 @@ let tPlanetes = [];
 let minuteriePrincipale = null;
 
 let indexPlanete = 0;
+let enMouvement = true;
 
 function assyncPlanete() {
 
@@ -40,8 +41,10 @@ function assyncPlanete() {
 
 //Fonction pour afficher les planètes selon le JSON envoyé
 
-function afficherPlanete() {
-
+function afficherPlanete(sens) {
+    if (sens !== false) {
+        incrementerIndex(1)
+    }
     let nomPlanete = document.querySelector("p");
     let imagePlanete = document.querySelector("img")
 
@@ -54,7 +57,6 @@ function afficherPlanete() {
     nomPlanete.innerHTML = tPlanetes[indexPlanete];
     imagePlanete.src = './images/' + tPlanetes[indexPlanete] + '.jpg'
 
-    incrementerIndex(1)
 
 }
 
@@ -66,7 +68,7 @@ function incrementerIndex(sens) {
         indexPlanete++;
     }
     if (sens === -1) {
-        indexPlanete--;
+        indexPlanete = indexPlanete - 1;
     }
 
     if (indexPlanete > tPlanetes.length - 1) {
@@ -74,8 +76,55 @@ function incrementerIndex(sens) {
     }
 
     if (indexPlanete < 0) {
-        indexPlanete = tPlanetes.length -1;
+        indexPlanete = tPlanetes.length - 1;
     }
 
 }
 
+// Fonction pour aller à l'image suivante et l'image précédente et arrêter
+
+const btnPrecedent = document.getElementById("precedentBtn");
+const btnSuivant = document.getElementById("suivantBtn");
+const btnStop = document.getElementById("stopBtn");
+
+btnSuivant.addEventListener("click", () => {
+    incrementerIndex(1);
+    afficherPlanete()
+})
+
+btnPrecedent.addEventListener("click", () => {
+    incrementerIndex(-1);
+    afficherPlanete(false)
+})
+
+btnStop.addEventListener("click", () => {
+    gestionVisionneuse()
+})
+
+// Fonction pour arrêter ou démarrer la visionneuse
+
+function gestionVisionneuse() {
+
+    if (btnStop.value === "stop") {
+        clearInterval(minuteriePrincipale);
+        minuteriePrincipale = "";
+        afficherVitesse(true);
+
+        btnStop.innerHTML = "Démarrer";
+        btnStop.value = "demarrer"
+    } else {
+        minuteriePrincipale = setInterval(afficherPlanete, 1000);
+        afficherVitesse(false);
+
+        btnStop.innerHTML = "Stop";
+        btnStop.value = "stop"
+    }
+}
+
+// Fonction pour afficher le select de vitesse ou le retirer
+
+const selectVitesse = document.getElementById("select-vitesse")
+
+function afficherVitesse(afficher) {
+    selectVitesse.hidden = !afficher;
+}
