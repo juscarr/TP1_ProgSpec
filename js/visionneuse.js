8cd94ee3f12a89e82cbabbcceab6f671b6a9ecb0
Justin Carrier <2162774@csfoy.ca>
2023-10-19 07:53:07 -0400

    1er fonctionnalité finalisé

--- js/visionneuse.js
+++ js/visionneuse.js
@@ -13,12 +13,6 @@ function assyncPlanete() {
 
     filmActuel = selectFilm.value;
 
-    // arretJouerBtn.innerHTML = "Stop";
-    // arretJouerBtn.value = "Stop";
-
-    // deffilementAuto = true;
-    // mySelect2.style.display = "none";
-
     fetch("https://swapi.dev/api/films/" + filmActuel)
         .then((response) => response.json())
         .then((json) => {
@@ -52,44 +46,3 @@ function afficherPlanete() {
 }
 
 
-const mySelect2 = document.getElementById("select-vitesse");
-
-// const precedentBtn = document.getElementById("precedentBtn");
-// const suivantBtn = document.getElementById("suivantBtn");
-// const arretJouerBtn = document.getElementById("arretBtn");
-
-let minuterie = null;
-let tPlanets = [];
-let index = 0;
-let deffilementAuto = true;
-let filmActuel = "";
-
-
-function gererCarrouselImg() {
-    clearInterval(minuterie);
-    if (mySelect2.value === "lent") {
-        minuterie = setInterval(() => {
-            index = (index + 1) % tPlanets.length;
-            document.querySelector("p").innerHTML = tPlanets[index].name;
-            document.querySelector(
-                "img"
-            ).src = `./images/${tPlanets[index].name}.jpg`;
-        }, 1500);
-    } else if (mySelect2.value === "moyen") {
-        minuterie = setInterval(() => {
-            index = (index + 1) % tPlanets.length;
-            document.querySelector("p").innerHTML = tPlanets[index].name;
-            document.querySelector(
-                "img"
-            ).src = `./images/${tPlanets[index].name}.jpg`;
-        }, 1000);
-    } else if (mySelect2.value === "rapide") {
-        minuterie = setInterval(() => {
-            index = (index + 1) % tPlanets.length;
-            document.querySelector("p").innerHTML = tPlanets[index].name;
-            document.querySelector(
-                "img"
-            ).src = `./images/${tPlanets[index].name}.jpg`;
-        }, 500);
-    }
-};
